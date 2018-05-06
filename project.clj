973f09c6af175e3e80f0626a7dbe1f0610346f23
Michiel Borkent <michielborkent@gmail.com>
2018-05-06 13:40:26 +0200

    Update README

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cljtree-graalvm "0.1.0-SNAPSHOT"
-  :description "Clojure formatter using cljfmt built with GraalVM"
-  :url "https://gitlab.com/konrad.mrozek/cljfmt-graalvm"
+  :description "Tree command in Clojure"
+  :url "https://github.com/borkdude/cljtree-graalvm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
